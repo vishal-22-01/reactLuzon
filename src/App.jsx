9e9b8dec5101388d51405140067aeef7f956677a
Shadab <shadab.murtaza@itechnolabs.tech>
2025-04-28 18:21:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+   <>
+    < Navbar />
+   </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar_container">
+        <div className="container">
+            <div className="custom_navbar">
+          {/* <div className="nav_logo">{Logo}</div> */}
+          <img src="/images/logo.png" alt="gf" />
+          <div className="navbar_links">
+            <a className="navbar_link" href="#">
+                <div className="dropdown">
+                    <span>Home</span>
+                    <div className="dropdown_menu d-flex flex-column gap-3">
+                        <a href="#">Demo-1</a>
+                        <a href="#">Demo-2</a>
+                        <a href="#">Demo-3</a>
+                        <a href="#">Demo-4</a>
+                        <a href="#">Demo-5</a>
+                        <a href="#">Demo-6</a>
+                        <a href="#">Demo-7</a>
+                    </div>
+                </div>
+                </a>
+          </div>
+            </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
